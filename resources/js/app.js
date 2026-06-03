22a937d494c3e6440355426ce3afcbf5701ecd37
KINHA Fatime <kinhafatime@gmail.com>
2026-06-03 21:20:02 +0100

    Netlify configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,6 @@ import App from './App.vue'
 
 import '../css/app.css'
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
 const app = createApp(App)
 
 app.mount('#app')
-
-AOS.init({
-    duration: 1000,
-    once: true
-})
